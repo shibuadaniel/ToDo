088c3af9a4bcb37c5b0c01d29999439b99f09a4c
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-10-12 13:07:09 -0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by SHIBU DANIEL on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
